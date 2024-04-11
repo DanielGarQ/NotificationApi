af458c0b9b53f34af5114f8b943886f8faf9a2f0
Alejandro Gómez Orjuela <102133299+alejoDev117@users.noreply.github.com>
2024-04-10 20:30:59 -0500

    .

--- src/main/java/com/notificationapi/notificationapi/service/UsuarioService.java
+++ src/main/java/com/notificationapi/notificationapi/service/UsuarioService.java
@@ -9,11 +9,14 @@ import com.notificationapi.notificationapi.entity.UsuarioEntity;
 import com.notificationapi.notificationapi.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.UUID;
 
-@Repository
+@Service
+@Transactional
 public class UsuarioService {
     @Autowired
     UsuarioRepository usuarioRepository;
